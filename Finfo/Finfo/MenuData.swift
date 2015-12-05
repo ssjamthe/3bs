2b4d0c80e7d648f244ec990d089eaada09f61e29
Swapnil Jamthe <swapnil@Swapnils-MacBook-Pro.local>
2015-12-06 02:40:55 +0530

    Removed dynamic menus as there were unwanted menus

--- Finfo/Finfo/MenuData.swift
+++ Finfo/Finfo/MenuData.swift
@@ -13,6 +13,12 @@ class MenuData
     let banksComparisonMenuData = BanksComparisonMenuData()
     let banksMenuData = BanksMenuData()
     let productsMenuData = ProductsMenuData()
+    
+    
     var bankNames:[String] = ["ICICI","AXIS","CITI","SBI","ABN Amro","Punjab Bank","Bank Of Maharashtra"]
     var productSubproductsMap: [String:Set<String>] = ["Credit Card":["Gold","Platinum","Silver"],"Personal Loan":["Big Loan","Small Loan","Long Term Loan","Short Term Loan"],"Home Loan":["Home Loan 1","Home Loan 2","Home Loan 3"],"Insurance":["Insurance One","Insurance 2","Insurance 3"]]
+    
+    var selectedProduct:String = "Credit Card"
+    var selectedBanks:Set<String> = Set<String>()
+    var selectedSubProducts:Set<String> = Set<String>()
 }
--- Finfo/Finfo/MenuUIViewController.swift
+++ Finfo/Finfo/MenuUIViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class MenuUIViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+class MenuUIViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate {
     
+    @IBOutlet weak var productTextInput: UITextField!
+    @IBOutlet weak var menuList: UITableView!
     @IBOutlet weak var dataTypePicker: UIPickerView!
-    
     @IBOutlet weak var dataReprsentationPicker: UIPickerView!
     
-    let dataTypePickerOptions=[["Compare Banks","Banks","Products"]]
+    var productTextInputClicked = false
+    var bankPlusClicked = false
+    var subproductPlusClicked = false
     
     let dataRepresentationPickerOptions=[["Pie Chart","Line Chart","Tabular Data","Row Data"]]
+    
+    
+    let dataTypePickerOptions=[["Complaints","Timely Response Percent","Disputed Response Percent"]]
     
     var menuData : MenuData!
     
@@ -29,31 +35,153 @@ class MenuUIViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDa
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataTypePicker.delegate = self
-        dataTypePicker.dataSource = self
-        dataReprsentationPicker.delegate = self
-        dataReprsentationPicker.dataSource = self
-        showBanksComparisonMenu()
+        
+        self.menuList.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.menuList.delegate = self
+        self.menuList.dataSource = self
+        self.menuList.hidden = true
+        
+        self.dataReprsentationPicker.delegate = self
+        self.dataReprsentationPicker.dataSource = self
+        
+        self.dataTypePicker.delegate = self
+        self.dataTypePicker.dataSource = self
+        
+        self.productTextInput.delegate = self
+        
+        self.productTextInput.text = self.menuData.selectedProduct
         
         print("Inside MenuUIViewController.viewDidLoad ")
         
     }
     
     
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if bankPlusClicked
+        {
+            return self.menuData.bankNames.count
+        }else if productTextInputClicked
+        {
+            return Array(self.menuData.productSubproductsMap.keys).count
+        }
+        else if subproductPlusClicked
+        {
+            return self.menuData.productSubproductsMap[self.menuData.selectedProduct]!.count
+        }
+        else
+        {
+            return 0
+        }
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        return false
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = self.menuList.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
+        
+        if bankPlusClicked
+        {
+            cell.textLabel?.text = self.menuData.bankNames[indexPath.row]
+            if self.menuData.selectedBanks.contains(self.menuData.bankNames[indexPath.row])
+            {
+                cell.selected = true
+                self.menuList.selectRowAtIndexPath(indexPath, animated:false,scrollPosition:UITableViewScrollPosition.None)
+            }
+        }
+        else if productTextInputClicked
+        {
+            cell.textLabel?.text = Array(self.menuData.productSubproductsMap.keys)[indexPath.row]
+            if self.menuData.selectedProduct == Array(self.menuData.productSubproductsMap.keys)[indexPath.row]
+            {
+                cell.selected = true
+                self.menuList.selectRowAtIndexPath(indexPath, animated:false,scrollPosition:UITableViewScrollPosition.None)
+                
+            }
+            
+        }else if subproductPlusClicked
+        {
+            let subProductName = Array(self.menuData.productSubproductsMap[self.menuData.selectedProduct]!)[indexPath.row]
+            cell.textLabel?.text = subProductName
+            if self.menuData.selectedSubProducts.contains(subProductName)
+            {
+                cell.selected = true
+                self.menuList.selectRowAtIndexPath(indexPath, animated:false,scrollPosition:UITableViewScrollPosition.None)
+                
+            }
+        }
+        
+        print(cell.textLabel?.text)
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        print(indexPath.row)
+        
+        if bankPlusClicked
+        {
+            
+            self.menuData.selectedBanks.insert(self.menuData.bankNames[indexPath.row])
+            
+            
+        }else if productTextInputClicked
+        {
+            let product = Array(self.menuData.productSubproductsMap.keys)[indexPath.row]
+            
+            self.menuData.selectedProduct = product
+            self.productTextInput.text = product
+            self.menuList.hidden = true
+            
+        }else if subproductPlusClicked
+        {
+            let subproduct = Array(self.menuData.productSubproductsMap[self.menuData.selectedProduct]!)[indexPath.row]
+            print("selected subproduct " + subproduct)
+            
+            self.menuData.selectedSubProducts.insert(subproduct)
+            
+        }
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.row)
+        if bankPlusClicked
+        {
+            self.menuData.selectedBanks.remove(self.menuData.bankNames[indexPath.row])
+            
+        }else if productTextInputClicked
+        {
+            self.menuList.selectRowAtIndexPath(indexPath, animated:false,scrollPosition:UITableViewScrollPosition.None)
+            self.menuList.hidden = true
+        }else if subproductPlusClicked
+        {
+            let subproduct = Array(self.menuData.productSubproductsMap[self.menuData.selectedProduct]!)[indexPath.row]
+            self.menuData.banksComparisonMenuData.selectedSubProducts.remove(subproduct)
+        }
+    }
+    
     override func viewDidDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
         
         print("Inside MenuUIViewController.viewDidDisappear ")
-
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        if pickerView == dataTypePicker
+        
+        if(pickerView == dataTypePicker)
         {
             return dataTypePickerOptions.count
         }
@@ -64,101 +192,93 @@ class MenuUIViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDa
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == dataTypePicker
+        
+        if(pickerView == dataTypePicker)
         {
+            
             return dataTypePickerOptions[component].count
         }
         else
         {
             return dataRepresentationPickerOptions[component].count
         }
+        
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == dataTypePicker
+        
+        if(pickerView == dataTypePicker)
         {
+            
             return dataTypePickerOptions[component][row]
         }
         else
         {
             return dataRepresentationPickerOptions[component][row]
         }
+        
     }
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerView == dataTypePicker
-        {
-            
-            if row == 0
-            {
-                showBanksComparisonMenu()            }
-            else if row == 1
-            {
-                showBanksMenu()
-            }
-            else
-            {
-                showProductsMenu()
-            }
-        }
     }
     
-    func showBanksComparisonMenu()
-    {
-        banksComparisonMenu.hidden=false
-        banksMenu.hidden=true
-        productsMenu.hidden=true
-    }
-    
-    func showBanksMenu()
-    {
-        banksComparisonMenu.hidden=true
-        banksMenu.hidden=false
-        productsMenu.hidden=true
-    }
-    
-    func showProductsMenu()
-    {
-        banksComparisonMenu.hidden=true
-        banksMenu.hidden=true
-        productsMenu.hidden=false
-    }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-        if (segue.identifier == "banksComparisonMenuSegue") {
-            print("Inside MenuUIViewController if banksComparisonMenuSegue")
-            let svc = segue.destinationViewController as! BanksComparisonMenuViewController;
-            svc.menuData = menuData
-            
-        }else if (segue.identifier == "productsMenuSegue") {
-            print("Inside MenuUIViewController if productsMenuSegue")
-            let svc = segue.destinationViewController as! ProductsMenuViewController;
-            svc.menuData = menuData
-            
-        }else if (segue.identifier == "banksMenuSegue") {
-            print("Inside MenuUIViewController if banksMenuSegue")
-            let svc = segue.destinationViewController as! BanksMenuViewController;
-            svc.menuData = menuData
-            
-        }
-        else if(segue.identifier == "mainPageSegue") {
+        if(segue.identifier == "mainPageSegue") {
             let svc = segue.destinationViewController as! ViewController;
             svc.menuData = menuData
             
         }
     }
-
     
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func banksPlusAction(sender: AnyObject) {
+        
+        if bankPlusClicked
+        {
+            self.menuList.hidden = true
+            bankPlusClicked = false
+        }
+        else
+        {
+            bankPlusClicked = true
+            productTextInputClicked = false
+            subproductPlusClicked = false
+            self.menuList.reloadData()
+            self.menuList.hidden = false
+        }
     }
-    */
-
+    
+    @IBAction func touchDownProductTextAction(sender: UITextField) {
+        
+        print("Inside touchDownBankTextAction")
+        productTextInputClicked = true
+        
+        bankPlusClicked = false
+        subproductPlusClicked = false
+        
+        self.menuList.reloadData()
+        self.menuList.hidden = false
+        
+        
+    }
+    
+    
+    @IBAction func subProductPlusAction(sender: AnyObject) {
+        
+        if subproductPlusClicked
+        {
+            self.menuList.hidden = true
+            subproductPlusClicked = false
+        }
+        else
+        {
+            bankPlusClicked = false
+            productTextInputClicked = false
+            subproductPlusClicked = true
+            self.menuList.reloadData()
+            self.menuList.hidden = false
+        }
+    }
+    
 }
