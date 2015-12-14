480a8b3a03aba5a5962fb06d8289a1a014984f63
ssjamthe@gmail.com <ssjamthe@gmail.com>
2015-12-15 00:48:14 +0530

    Downloading data file

--- finfoBackend/src/main/java/com/finfo/config/AppInitializer.java
+++ finfoBackend/src/main/java/com/finfo/config/AppInitializer.java
@@ -3,9 +3,12 @@ package com.finfo.config;
 import javax.servlet.ServletContext;  
 import javax.servlet.ServletException;  
 import javax.servlet.ServletRegistration.Dynamic;  
+
 import org.springframework.web.WebApplicationInitializer;  
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
 import org.springframework.web.servlet.DispatcherServlet;  
+
+import com.finfo.schedular.DataReaderWriter;
 
 public class AppInitializer implements WebApplicationInitializer{
 
@@ -17,6 +20,8 @@ public class AppInitializer implements WebApplicationInitializer{
 	        Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
 	        dynamic.addMapping("/");  
 	        dynamic.setLoadOnStartup(1);
+	        DataReaderWriter dataReaderWriter = new DataReaderWriter();
+	        dataReaderWriter.readData();
 	}
 
 }
